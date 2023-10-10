90a1456e074ddbab54e3b4538b0cebb8b61ac190
Nicklas Boman <smurfd@gmail.com>
2023-10-10 21:24:52 +0200

    timing test

---
+++ test/test_timing.py
@@ -0,0 +1,13 @@
+import numpy as np
+import lightnum.lightnum as lp
+from datetime import datetime 
+
+start_time = datetime.now() 
+for _ in range(10000): np.mod([1, 2, 3, 4], [1, 2, 6, 4])
+time_elapsed = datetime.now() - start_time 
+print('NP Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
+
+start_time = datetime.now() 
+for _ in range(10000): lp.mod([1, 2, 3, 4], [1, 2, 6, 4])
+time_elapsed = datetime.now() - start_time 
+print('LP Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
