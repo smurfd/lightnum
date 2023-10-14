565eae2bb3a3c159c6e56b6d1ddd04a66e7b83a5
Nicklas Boman <smurfd@gmail.com>
2023-10-14 23:16:33 +0200

    better timing

--- test/test_timing.py
+++ test/test_timing.py
@@ -1,31 +1,50 @@
 import numpy as np
 import lightnum.lightnum as lp
-from datetime import datetime
+import time
 
-def test_timing():
-  start_time = datetime.now()
-  for _ in range(1000000): np.mod([1, 2, 3, 4], [1, 2, 6, 4])
-  time_elapsed1 = datetime.now() - start_time
-  print('NP Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed1))
+def test_timing_mod():
+  start = time.time()
+  for _ in range(100000): np.mod([1, 2, 3, 4], [1, 2, 6, 4])
+  timer1 = (time.time() - start) * 1000
+  print('Numpy mod: {0:.4f}ms'.format(timer1))
 
-  start_time = datetime.now()
-  for _ in range(1000000): lp.mod([1, 2, 3, 4], [1, 2, 6, 4])
-  time_elapsed2 = datetime.now() - start_time
-  print('LP Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed2))
-  assert(time_elapsed2 < time_elapsed1)
-
-  start_time = datetime.now()
-  for _ in range(1000000): np.sqrt([1, 2, 3, 4])
-  time_elapsed1 = datetime.now() - start_time
-  print('NP Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed1))
-
-  start_time = datetime.now()
-  for _ in range(1000000): lp.sqrt([1, 2, 3, 4])
-  time_elapsed2 = datetime.now() - start_time
-  print('LP Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed2))
-  assert(time_elapsed2 < time_elapsed1)
+  start = time.time()
+  for _ in range(100000): lp.mod([1, 2, 3, 4], [1, 2, 6, 4])
+  timer2 = (time.time() - start) * 1000
+  print('Lightnum mod: {0:.4f}ms\n'.format(timer2))
+  assert(timer2 < timer1)
   # Faster than numpy!!!
-  # NP Time elapsed (hh:mm:ss.ms) 0:00:01.323887
-  # LP Time elapsed (hh:mm:ss.ms) 0:00:01.178521
-test_timing()
+  # Numpy mod: 147.8453ms
+  # Lightnum mod: 121.3479ms
+
+def test_timing_sqrt():
+  start = time.time()
+  for _ in range(100000): np.sqrt([1, 2, 3, 4])
+  timer1 = (time.time() - start) * 1000
+  print('Numpy sqrt: {0:.4f}ms'.format(timer1))
+
+  start = time.time()
+  for _ in range(100000): lp.sqrt([1, 2, 3, 4])
+  timer2 = (time.time() - start) * 1000
+  print('Lightnum sqrt: {0:.4f}ms\n'.format(timer2))
+  assert(timer2 < timer1)
+  # Faster than numpy!!!
+  # Numpy sqrt: 93.0297ms
+  # Lightnum sqrt: 81.8470ms
+
+def test_timing_assert():
+  start = time.time()
+  for _ in range(100000): np.testing.assert_equal([1, 2, 3, 4], [1, 2, 3, 4])
+  timer1 = (time.time() - start) * 1000
+  print('Numpy assert_equal: {0:.4f}ms'.format(timer1))
+
+  start = time.time()
+  for _ in range(100000): lp.testing.assert_equal([1, 2, 3, 4], [1, 2, 3, 4])
+  timer2 = (time.time() - start) * 1000
+  print('Lightnum assert_equal: {0:.4f}ms\n'.format(timer2))
+  assert(timer2 < timer1)
+
+test_timing_mod()
+test_timing_sqrt()
+test_timing_assert()
 print("OK!")
