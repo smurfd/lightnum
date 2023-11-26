e079d740bb0f16bb5d9d2e8deb8fe6e58168c3f9
Nicklas Boman <smurfd@gmail.com>
2023-11-26 03:08:26 +0100

    create portable

--- scripts/create_portable.py
+++ scripts/create_portable.py
@@ -4,16 +4,21 @@ def create_portable_getdata(fn):
   data = []
   with open(fn, "r") as f:
     for line in f:
-      if not line.startswith("from lightnum"):
+      if not line.startswith("from lightnum"): # We dont need those imports since its all in one file
         fnd = re.search(r"from lightnum.[\Sa-z]*", line)
         if fnd: line = line.replace(line[fnd.span()[0]:fnd.span()[1]], "from lightnum")
         data.append(line)
   return data
 
 def create_portable_writedata(fn, data):
+  imp = []; dat = []
+  for d in data:
+    if d.startswith("import"): imp.append(d)
+    else: dat.append(d)
+  imp = list(set(imp)) # hack to get unique list
+  for i, u in enumerate(imp): dat.insert(i, u)
   with open(fn, "w") as f:
-    for d in data:
-      f.write(str(d))
+    for d in dat: f.write(str(d))
 
 if __name__ == '__main__':
   data = []
