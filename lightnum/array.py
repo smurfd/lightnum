63879875bba6d466c1ac3a5c5e503150f2e2ac40
Nicklas Boman <smurfd@gmail.com>
2024-01-21 22:26:43 +0100

    array

--- lightnum/array.py
+++ lightnum/array.py
@@ -3,16 +3,15 @@ class array(object):
   def __getattr__(self, name): return getattr(self.x, name)
   def __repr__(self): return repr(self.x)
   def __str__(self): return str(self.x)
-  #def flatten(self, x): return helper.reshape(x, -1)
-  def astype(self, x): return [x(self.x[i]).value for i in range(len(self.x))]
-  def tolist(self): return list(self.x)
-  def numpy(self): return list(self.x)
   def __getitem__(self, itm): return self.x.__getitem__(itm)
   def __sub__(self, x): return ndarray([i - x for i in self.x])
   def __truediv__(self, x): return [i / x for i in self.x]
   def __floordiv__(self, x): return [i // x for i in self.x]
   def __call__(self, x): return ndarray(x)
   def __len__(self, x): return len(self.x) if "Image" not in str(type(x)) else len(x.size)
+  def astype(self, x): return [x(self.x[i]).value for i in range(len(self.x))]
+  def tolist(self): return list(self.x)
+  def numpy(self): return list(self.x)
 
 class asarray(array): pass
 
@@ -23,4 +22,6 @@ class ndarray(array):
       else: self.x = [0 for a in range(len(x)) for b in range(len(x[a])) if a != b]
     else: self.x = [x[i] for i in range(len(x))]
   def __len__(self): return len(self.x)
+  def __truediv__(self, x): return [i // x for i in self.x]
+  #def __getitem__(self, itm): return ndarray(self.x.__getitem__(itm))
   def astype(self, x): return [0 for _ in range(self.x[0])] if len(self.x) == 1 else [x(self.x[i]).value for i in range(len(self.x))]
