1fc796e7b06cda8bbc143529624562ef458dd6be
Nicklas Boman <smurfd@gmail.com>
2024-02-06 21:16:22 +0100

    typing, helper

--- lightnum/helper.py
+++ lightnum/helper.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 from __future__ import annotations
-from typing import List, Any, Callable, Type, Optional
+from typing import List, Any, Callable, Type, Optional, BinaryIO, Tuple, Union, Dict
 from lightnum.dtypes import int16, int32, uint32, float16, float32, float64, uint8, uint16, types, dtype
 from lightnum.array import ndarray, array
 import itertools, builtins, ctypes, struct, math
 
 class helper:
   MAGIC_PREFIX = b'\x93NUMPY'
-  _header_size_info = {(1, 0): ('<H', 'latin1'), (2, 0): ('<I', 'latin1'), (3, 0): ('<I', 'utf8')}
+  _header_size_info: Dict[Tuple[int, int], Tuple[str, str]] = {(1, 0): ('<H', 'latin1'), (2, 0): ('<I', 'latin1'), (3, 0): ('<I', 'utf8')}
   def cbrt(self, x: int) -> Any: return round(x**(1 / 3.), 2)
   def format_float(self, x: float) -> Any: return float(('%i' if x == int(x) else '%s') % x)
   def cast(self, x: List[Any], dtype:dtype = float64) -> Any: return self.looper_cast(x, dtype=dtype) if str(dtype) != types[dtype(x)] else x
@@ -33,43 +33,45 @@ class helper:
   def looper_transpose(self, x: List[Any], axes: Optional[List[Any]]=None) -> List[Any]: return [[row[i] for row in x] for i in range(len(x[0]))] #TODO: axes
   def looper_stack(self, x: List[Any], axis: int = 0) -> List[Any]: return [i for i in x] #TODO: axis
   def looper_expand_dims(self, x: List[Any], axis: int, axisco: int = 0) -> List[Any]: return [x] if axisco == axis else [self.looper_expand_dims(x[i], axis, axisco + 1) for i in range(len(x))]
-  def looper_flip(self, x: List[Any], dtype: Type[Any] = int32) -> List[Any]: return [self.looper_flip(i) for i in x] if isinstance(x[0], list) else x[::-1]
-  def looper_squeeze(self, x, axis=0) -> List[Any]: return x if not isinstance(x[0][0], list) else [self.looper_squeeze(y, axis) for y in x].pop() #TODO: axis
-  def looper_clip(self, x, x_min, x_max) -> float|int: return (x_min if x < x_min else x_max) if not isinstance(x, list) else [self.looper_clip(y, x_min, x_max) for y in x]
-  def looper_broadcast_to(self, x, y) -> List[Any]: return x if len(y) == 1 else [x for i in range(y[0])]
-  def looper_nonzero(self, x):
-    ret1, ret2 = [], []
-    if isinstance(x[0], list): [[(ret1.append(i), ret2.append(ii)) for ii in range(len(x[i])) if x[i][ii]] for i in range(len(x)) if isinstance(x[i], list)]
+  def looper_flip(self, x: List[Any], dtype: dtype = int32) -> List[Any]: return [self.looper_flip(i) for i in x] if isinstance(x[0], list) else x[::-1]
+  def looper_squeeze(self, x: List[Any], axis: int = 0) -> List[Any]: return x if not isinstance(x[0][0], list) else [self.looper_squeeze(y, axis) for y in x].pop() #TODO: axis
+  def looper_clip(self, x: List[Any]| int, x_min: int, x_max: int) -> Union[Any, Any]: return (x_min if x < x_min else x_max) if not isinstance(x, list) else [self.looper_clip(y, x_min, x_max) for y in x]
+  def looper_broadcast_to(self, x: List[Any], y: List[Any]) -> List[Any]: return x if len(y) == 1 else [x for i in range(y[0])]
+  def looper_nonzero(self, x: List[Any]) -> Tuple[ndarray, ...]:
+    ret1: List[Any] = []
+    ret2: List[Any] = []
+    if isinstance(x[0], list): [[(ret1.append(i), ret2.append(ii)) for ii in range(len(x[i])) if x[i][ii]] for i in range(len(x)) if isinstance(x[i], list)] # type: ignore
     return tuple([ndarray(ret1)] + [ndarray(ret2)])
 
-  def looper_arange(self, start, stop=0, step=1, dtype=int32):
+  def looper_arange(self, start: int, stop: int = 0, step: int = 1, dtype: dtype = int32) -> List[Any] | ndarray:
     if stop: return ndarray([i for i in range(start, stop, step)])
     return ndarray([i for i in range(0, start, step)])
 
-  def looper_argmax(self, x, axis=None):
+  def looper_argmax(self, x: List[Any], axis: int = 0) -> int | List[Any]:
     if not axis:
       y = self.reshape(x, -1)
       return y.index(max(y))
     return x.index(max(x)) if not isinstance(x[0], list) else [self.looper_argmax(y, axis) for y in x]
 
-  def looper_cumsum(self, x, s = 0, dtype = int32):
+  def looper_cumsum(self, x: List[Any], s: int = 0, dtype: dtype = int32) -> List[Any]:
     a = self.reshape(x, -1)
     return [builtins.sum(a[0:i:1]) for i in range(0, len(a)+1)][1:]
 
-  def looper_median(self, x, dtype=int32):
+  def looper_median(self, x: List[Any], dtype: dtype = int32) -> int:
     ret = 0
     for i in range(len(x)):
       if isinstance(x[i], (list, tuple)): ret = self.looper_median(x[i])
       else: ret += x[i]
     return ret
 
-  def looper_split(self, x, y, dtype=int32):
+  def looper_split(self, x: List[Any], y: List[Any], dtype: dtype = int32) -> List[Any]:
     if not isinstance(y, list) and not isinstance(x, list): return [ndarray(x.tolist()[i:i+len(x.tolist())//y]) for i in range(0, len(x.tolist()), len(x.tolist())//y)]
     elif not isinstance(y, list): return [ndarray(x[i:i+(len(x)//y)]) for i in range(0, len(x), len(x)//y)]
     return [self.looper_split(i, y) for i in x]
 
-  def looper_tile(self, x, y, dtype=int32):
-    tmp, tmp2=[], []
+  def looper_tile(self, x: List[Any], y: Tuple[Any, Any], dtype: dtype = int32) -> List[Any]:
+    tmp: List[Any] = []
+    tmp2: List[Any] = []
     if not isinstance(y, tuple):
       for _ in range(y): tmp.extend(x)
       return ndarray(tmp)
@@ -80,11 +82,13 @@ class helper:
       tmp2=[]
     return tmp
 
-  def looper_empty(self, x, fill, dtype=int32):
+  def looper_empty(self, x: List[Any], fill: float| int, dtype: dtype = int32) -> List[Any]:
     if isinstance(x, (int, float)): return [fill]*x
     elif len(x)==1: return([fill]*x[0])
     elif isinstance(x, (list, tuple)) and isinstance(x[0], (list, tuple)): return [self.looper_empty(x[i], fill=fill, dtype=dtype) for i in range(len(x))]
-    r, ret, ret1 =[], [], []
+    r: List[Any] = []
+    ret: List[Any] = []
+    ret1: List[Any] = []
     for j, idx in enumerate(itertools.product(*[range(s) for s in x[1::]])): #start from 1 since we will fill a row of x[0] len for each
       if not j%(x[0]) and j:
         ret.append(ret1)
@@ -102,25 +106,25 @@ class helper:
     if not r and not ret: r.extend(ret1)
     return r
 
-  def looper_empty_like(self, x, fill, dtype=int32):
+  def looper_empty_like(self, x: List[Any], fill: float | int, dtype: dtype = int32) -> List[Any]:
     if isinstance(x, int): return [fill] * x
     elif isinstance(x, list):
       if not isinstance(x[0], list): return [fill] * len(x)
       return [fill for a in range(len(x)) for b in range(len(x[a]))]
 
-  def looper_max(self, x, ret=0):
+  def looper_max(self, x: List[Any], ret: float | int | List[Any] = 0) -> float | int | List[Any]:
     for i in range(len(x)):
       if isinstance(x[i], (list, tuple)): ret = self.looper_max(x[i], ret)
       elif ret <= x[i]: ret = x[i]
     return ret
 
-  def looper_min(self, x, ret=0):
+  def looper_min(self, x: List[Any], ret: float | int | List[Any] = 0) -> float | int | List[Any]:
     for i in range(len(x)):
       if isinstance(x[i], (list, tuple)): ret = self.looper_min(x[i], ret)
       elif ret >= x[i]: ret = x[i]
     return ret
 
-  def looper_maximum(self, x, y):
+  def looper_maximum(self, x: List[Any], y: List[Any]) -> List[Any]:
     tmp, ret = [], []
     for i in range(len(x)):
       if builtins.all(isinstance(j, list) for j in [x[i],y[i]]): tmp.append(self.looper_maximum(x[i], y[i]))
@@ -128,7 +132,7 @@ class helper:
     ret.extend(tmp)
     return ret
 
-  def looper_minimum(self, x, y):
+  def looper_minimum(self, x: List[Any], y: List[Any]) -> List[Any]:
     tmp, ret = [], []
     for i in range(len(x)):
       if builtins.all(isinstance(j, list) for j in [x[i],y[i]]): tmp.append(self.looper_minimum(x[i], y[i]))
@@ -136,7 +140,7 @@ class helper:
     ret.extend(tmp)
     return ret
 
-  def looper_isin(self, x, y):
+  def looper_isin(self, x: List[Any], y: List[Any]) -> List[Any]:
     ret = []
     for i in range(len(x)):
       if isinstance(y, list) and builtins.all(isinstance(j, list) for j in [x[i],y[i]]): ret = self.looper_isin(x[i], y[i])
@@ -145,13 +149,13 @@ class helper:
       else: ret.append(True) if x[i] == y[i] else ret.append(False)
     return ret
 
-  def looper_count(self, x, ret=0):
+  def looper_count(self, x: List[Any], ret: int = 0) -> int:
     for i in range(len(x)):
       if isinstance(x[i], (list, tuple)): ret = self.looper_count(x[i], ret)
       elif (x[i] != 0 and x[i] is not False): ret += 1
     return ret
 
-  def looper_matmul(self, x, y, dtype=int32):
+  def looper_matmul(self, x: List[Any], y: List[Any], dtype: dtype = int32) -> List[Any]:
     if not isinstance(x, (list, tuple)):
       if x and y: return x * y
       elif x and not y: return x
@@ -160,8 +164,8 @@ class helper:
     return self.reshape(ret, -1)
 
   # BARF
-  def looper_pad(self, x, y, mode='constant', **kwargs):
-    ret = []
+  def looper_pad(self, x: List[Any], y: List[Any] | Tuple[Any, Any], mode: str ='constant', **kwargs: Any) -> List[Any]:
+    ret: List[Any] = []
     app = [x[i] for i in range(len(x))]
     if mode == 'constant':
       ret.extend(kwargs['constant_values'][0] for i in range(y[0] if isinstance(y, tuple) else y))
@@ -195,14 +199,14 @@ class helper:
       ret.extend(app)
       ret.extend(maxv for i in range(y[1] if isinstance(y, tuple) else y))
     elif mode == 'mean':
-      meanv = 0
+      meanv: int | float = 0
       for i in range(len(x)): meanv += x[i]
       meanv = int(meanv / len(x))
       ret.extend(meanv for i in range(y[0] if isinstance(y, tuple) else y))
       ret.extend(app)
       ret.extend(meanv for i in range(y[1] if isinstance(y, tuple) else y))
     elif mode == 'median':
-      if isinstance(len(x)/2, int): meadv = x[len(x)/2]
+      if isinstance(len(x)/2, int): meadv = x[len(x)/2] # type: ignore
       else: meadv = int(math.ceil((x[int(len(x)/2)] + x[int(len(x)/2)-1]) / 2))
       ret.extend(meadv for i in range(y[0] if isinstance(y, tuple) else y))
       ret.extend(x[i] for i in range(len(x)))
@@ -232,8 +236,11 @@ class helper:
       ret.extend(None for i in range(y[1] if isinstance(y, tuple) else y))
     return ret
 
-  def reshape(self, col, shape):
-    ncols, nrows, ret, row = 0, 0, [], []
+  def reshape(self, col: List[Any], shape: int | List[int]) -> List[Any]:
+    ncols: int = 0
+    nrows: int = 0
+    ret: List[Any] = []
+    row: List[Any] = []
     if isinstance(shape, tuple): nrows, ncols = shape
     elif not isinstance(col, (list, tuple)): ncols, nrows = col, 1
     else: ncols, nrows = len(col), 1
@@ -253,21 +260,21 @@ class helper:
       row=[]
     return ret
 
-  def read_magic(self, f): return f.read(len(self.MAGIC_PREFIX)) == self.MAGIC_PREFIX
-  def read_header_type(self, f): return int.from_bytes(f.read(1), "big"), int.from_bytes(f.read(1), "big")
-  def read_header_pack(self, t): return self._header_size_info.get(t)[0]
-  def read_header_enc(self, t): return self._header_size_info.get(t)[1]
-  def read_header_len(self, f, p): return struct.unpack(self.read_header_pack(p), f.read(2))[0]
-  def read_header(self, f, t):
+  def read_magic(self, f: BinaryIO) -> Any: return f.read(len(self.MAGIC_PREFIX)) == self.MAGIC_PREFIX
+  def read_header_type(self, f: BinaryIO) -> Tuple[int, int]: return int.from_bytes(f.read(1), "big"), int.from_bytes(f.read(1), "big")
+  def read_header_pack(self, t: Tuple[int, int]) -> str: return self._header_size_info.get(t)[0] # type: ignore
+  def read_header_enc(self, t: Tuple[int, int]) -> str: return self._header_size_info.get(t)[1] # type: ignore
+  def read_header_len(self, f: BinaryIO, p: Tuple[int, int]) -> Any: return struct.unpack(self.read_header_pack(p), f.read(2))[0]
+  def read_header(self, f: BinaryIO, t: Tuple[int, int]) -> Tuple[Any, Any]:
     import ast
     h = f.read(self.read_header_len(f, t)).decode(self.read_header_enc(t))
-    return ast.literal_eval(h)['shape'], ast.literal_eval(h)['descr']
-  def read_body(self, f, l): return [int.from_bytes(f.read(8), "little") for _ in range(0, l, 8)]
+    return (ast.literal_eval(h)['shape'], ast.literal_eval(h)['descr'])
+  def read_body(self, f: BinaryIO, l: int) -> List[int]: return [int.from_bytes(f.read(8), "little") for _ in range(0, l, 8)]
 
-  def write_magic(self, f): f.write(self.MAGIC_PREFIX)
-  def write_header_type(self, f, t):
+  def write_magic(self, f: BinaryIO) -> None: f.write(self.MAGIC_PREFIX)
+  def write_header_type(self, f: BinaryIO, t: List[Any]) -> None:
     f.write(t[0].to_bytes(1, byteorder ='big'))
     f.write(t[1].to_bytes(1, byteorder ='big'))
-  def write_header_len(self, f, p, header): f.write(struct.pack('<H', len(header)))
-  def write_header(self, f, t, header): f.write(header.encode())
-  def write_body(self, f, x): [f.write(i.to_bytes(8,'little')) for i in x]
+  def write_header_len(self, f: BinaryIO, p: int, header: str) -> None: f.write(struct.pack('<H', len(header)))
+  def write_header(self, f: BinaryIO, t: str, header: str) -> None: f.write(header.encode())
+  def write_body(self, f: BinaryIO, x: List[Any]) -> None: [f.write(i.to_bytes(8,'little')) for i in x]
